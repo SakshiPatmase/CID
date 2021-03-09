e529211a400f02ff28f8628e53ea50240d76a1ba
SakshiPatmase <SakshiPatmase>
2021-03-09 13:36:22 +0530

    Project is complete

---
+++ test/com/demo/TestCalculator.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCalculator {
+
+	@Test
+	public void tesAdd()
+	{
+		BasicCalculator cal=new BasicCalculator();
+		int result=cal.add(1,2,3,4,5);
+		Assertions.assertEquals(15,result);
+	}
+}
